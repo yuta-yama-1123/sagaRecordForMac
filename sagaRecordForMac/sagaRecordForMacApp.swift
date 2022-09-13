27dfedb4f9ad3df2dc0edc27fe27214ed2b79201
yuta-yama-1123 <yutaforios@gmail.com>
2022-09-13 22:30:09 +0900

    Initial Commit

---
+++ sagaRecordForMac/sagaRecordForMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  sagaRecordForMacApp.swift
+//  sagaRecordForMac
+//
+//  Created by 山本祐太 on 2022/09/13.
+//
+
+import SwiftUI
+
+@main
+struct sagaRecordForMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
