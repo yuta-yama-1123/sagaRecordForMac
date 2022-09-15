f6eeedb74cd39c8fc672bbdd9d14a5d20f30a63a
yuta-yama-1123 <yutaforios@gmail.com>
2022-09-16 04:36:50 +0900

    フロントサイド実装9/16

---
+++ sagaRecordForMac/SignupView.swift
@@ -0,0 +1,58 @@
+import SwiftUI
+
+struct SignupView: View {
+  
+  @Binding var isAuthenticated: Bool
+  @Binding var isGoingToSignup: Bool
+  
+  @State var mailAddress: String = ""
+  @State var password: String = ""
+  
+  var body: some View {
+    VStack {
+      Text("you are going to sign up")
+      
+      VStack {
+        TextField("ログインID", text: $mailAddress)
+          .frame(width: 300)
+          .padding()
+          .font(
+            .system(size: 18, weight: .heavy, design: .rounded)
+          )
+        TextField("パスワード", text: $password)
+          .frame(width: 300)
+          .font(
+            .system(size: 16, weight: .heavy, design: .rounded)
+          )
+      }
+      .padding(EdgeInsets(top: 10, leading: 10, bottom: 40, trailing: 10))
+      
+      Button(action: {
+        signin()
+      }) {
+        Text("ログイン")
+          .font(
+            .system(size: 16, weight: .heavy, design: .rounded)
+          )
+      }
+    }
+    .frame(width: 450, height: 400)
+  }
+  
+  func signin() {
+    print("signin!!")
+  }
+}
+
+struct SignupView_Previews: PreviewProvider {
+  
+  @State static var isAuthenticated: Bool = false
+  @State static var isGoingToSignUp: Bool = false
+  
+  static var previews: some View {
+    SignupView(
+      isAuthenticated: $isAuthenticated,
+      isGoingToSignup: $isGoingToSignUp
+    )
+  }
+}
--- sagaRecordForMac/TopView.swift
+++ sagaRecordForMac/TopView.swift
@@ -3,12 +3,17 @@ import SwiftUI
 struct TopView: View {
   // ログイン成否
   @State var isAuthenticated: Bool = true
+  @State var isGoingToSignup: Bool = false
   
   var body: some View {
     VStack {
-      IndexView().sheet(isPresented: $isAuthenticated) {
-        SigninView(isAuthenticated: $isAuthenticated)
-      }
+      IndexView()
+        .sheet(isPresented: $isAuthenticated) {
+          SigninView(isAuthenticated: $isAuthenticated, isGoingToSignup: $isGoingToSignup)
+        }
+        .sheet(isPresented: $isGoingToSignup) {
+          SignupView(isAuthenticated: $isAuthenticated, isGoingToSignup: $isGoingToSignup)
+        }
     }
     .frame(width: 1000, height: 600)
   }
