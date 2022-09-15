37e6a6989f068d59e295dca9be7315061502b212
yuta-yama-1123 <yutaforios@gmail.com>
2022-09-16 04:36:27 +0900

    画面レイアウトのベースから少しずつ実装(9/16)

--- sagaRecordForMac/SigninView.swift
+++ sagaRecordForMac/SigninView.swift
@@ -1,28 +1,83 @@
 import SwiftUI
 
 struct SigninView: View {
+  
   @Binding var isAuthenticated: Bool
+  @Binding var isGoingToSignup: Bool
+  
+  @State var mailAddress: String = ""
+  @State var password: String = ""
+  
   var body: some View {
     VStack {
-      Text("Hello, world!")
+      Text("Welcome to SaGa Record for MAC")
+        .padding()
+        .font(
+          .system(size: 22, weight: .heavy, design: .rounded)
+        )
+      
+      VStack {
+        TextField("ログインID", text: $mailAddress)
+          .frame(width: 300)
           .padding()
+          .font(
+            .system(size: 18, weight: .heavy, design: .rounded)
+          )
+        TextField("パスワード", text: $password)
+          .frame(width: 300)
+          .font(
+            .system(size: 16, weight: .heavy, design: .rounded)
+          )
+      }
+      .padding(EdgeInsets(top: 10, leading: 10, bottom: 40, trailing: 10))
+      
       Button(action: {
-        login()
+        signin()
       }) {
         Text("ログイン")
+          .font(
+            .system(size: 16, weight: .heavy, design: .rounded)
+          )
       }
+      .buttonStyle(.borderless)
+      .frame(width: 130, height: 45)
+      .foregroundColor(Color.white)
+      .background(Color.blue)
+      .cornerRadius(10, antialiased: true)
+      .padding(EdgeInsets(top: 10, leading: 10, bottom: 20, trailing: 10))
+      
+      Button(action: {
+        signup()
+      }) {
+        Text("新規に登録される場合はこちら")
+          .font(
+            .system(size: 14, weight: .heavy, design: .rounded)
+          )
+      }
+      .buttonStyle(.borderless)
+      .foregroundColor(Color.blue)
     }
-    .frame(width: 250, height: 250)
+    .frame(width: 450, height: 400)
   }
   
-  func login() {
+  func signin() {
     isAuthenticated = false
+  }
+  
+  func signup() {
+    isAuthenticated = false
+    isGoingToSignup = true
   }
 }
 
 struct SigninView_Previews: PreviewProvider {
+  
   @State static var isAuthenticated: Bool = false
+  @State static var isGoingToSignUp: Bool = false
+  
   static var previews: some View {
-    SigninView(isAuthenticated: $isAuthenticated)
+    SigninView(
+      isAuthenticated: $isAuthenticated,
+      isGoingToSignup: $isGoingToSignUp)
   }
 }
