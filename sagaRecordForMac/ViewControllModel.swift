9c56704d11ed4a4de4a61204bc223af8ea5fa246
yuta-yama-1123 <yutaforios@gmail.com>
2022-09-20 21:58:47 +0900

    本日分の進捗をコミット09/20

--- sagaRecordForMac/ViewControllModel.swift
+++ sagaRecordForMac/ViewControllModel.swift
@@ -1,5 +1,19 @@
 import Foundation
+import SwiftUI
 
 class ViewControllModel {
+  @State var selectedView: String = ""
   
+  // ここに定義したメソッドを呼び出すことで各画面の表示/非表示をコントロールできるようにしたい
+  func editStatus() {
+    selectedView = "1"
+  }
+  
+  func searchStyles() {
+    selectedView = "2"
+  }
+  
+  func listStyles() {
+    selectedView = "3"
+  }
 }
