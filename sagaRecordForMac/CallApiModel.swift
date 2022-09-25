3848629f0e1203d561066edeb838139515a91e90
yuta-yama-1123 <yutaforios@gmail.com>
2022-09-26 01:12:05 +0900

    本日分の進捗をコミット09/26（GETリクエスト確認用処理追加）

--- sagaRecordForMac/CallApiModel.swift
+++ sagaRecordForMac/CallApiModel.swift
@@ -17,11 +17,19 @@ class CallAPIModel {
     print()
     return Promise { resolver in
       let requestUrl = constantValueModel.apiDomain + "/touch"
+      let param: Parameters = [
+        "hoge": "fuga",
+      ]
+      let headers: HTTPHeaders = [
+        "Content-Type":"application/x-www-form-urlencoded; charset=UTF-8"
+      ]
       AF // Alamofire
         .request(
           requestUrl,
           method: .post,
-          headers: HTTPHeaders.default
+          parameters: param,
+          encoding: URLEncoding.httpBody,
+          headers: headers
         )
         .validate(statusCode: 200..<300)
         .validate(contentType: ["application/json"])
@@ -34,6 +42,31 @@ class CallAPIModel {
             case .failure(let error):
               print("error:\(error)")
               resolver.reject(error)
+          }
+        }
+    }
+  }
+  
+  // GETお試し用
+  func callTouchGet() -> Promise<Bool> {
+    return Promise { resolver in
+      let requestUrl = constantValueModel.apiDomain + "/getTest"
+      let headers: HTTPHeaders = [
+        "Content-Type":"application/x-www-form-urlencoded; charset=UTF-8"
+      ]
+      AF // Alamofire
+        .request(
+          requestUrl,
+          method: .get,
+          headers: headers
+        )
+        .validate(statusCode: 200..<300)
+        .responseData { response in
+          switch response.result {
+          case .success(_):
+            resolver.fulfill(true)
+          case .failure(let error):
+            resolver.reject(error)
           }
         }
     }
