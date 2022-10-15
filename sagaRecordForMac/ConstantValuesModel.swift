497290f78eb54ea5e3335fc5fe117235e56cd29f
yuta-yama-1123 <yutaforios@gmail.com>
2022-10-15 23:45:27 +0900

    本日分の進捗10/15(検索条件のUIをどうにかしたい)

--- sagaRecordForMac/ConstantValuesModel.swift
+++ sagaRecordForMac/ConstantValuesModel.swift
@@ -11,6 +11,33 @@ class ConstantValuesModel {
   // もろもろ稼働確認用のAPIサーバドメイン
   let apiDomain: String = "https://nodejs-api-dev1123.herokuapp.com"
   
+  var series: [Series] = [
+      Series("RS1"),
+      Series("RS2"),
+      Series("RS3"),
+      Series("GB1"),
+      Series("GB2"),
+      Series("GB3"),
+      Series("ES"),
+      Series("US"),
+      Series("SSG"),
+      Series("RS"),
+      Series("Other")
+  ]
+  
+  var weapon: [Weapon] = [
+      Weapon("Sword"),
+      Weapon("GreatSword"),
+      Weapon("Axe"),
+      Weapon("Hummer"),
+      Weapon("Gun"),
+      Weapon("MartialArts"),
+      Weapon("Rapier"),
+      Weapon("Spear"),
+      Weapon("Arrow"),
+      Weapon("Wand")
+  ]
+  
   struct Result: Codable {
     var error: Bool
     var detail: [Base]
@@ -19,5 +46,29 @@ class ConstantValuesModel {
   struct Base: Codable {
       var value: Int              // BASE値
       var update_at: String       // 最終更新日
+  }
+  
+  // シリーズ管理
+  struct Series: Identifiable {
+    var id = UUID()
+    var isChecked: Bool
+    var name: String
+      
+    init(_ name: String) {
+      self.isChecked = false
+      self.name = name
+    }
+  }
+  
+  // 武器種管理
+  struct Weapon: Identifiable {
+    var id = UUID()
+    var isChecked: Bool
+    var name: String
+      
+    init(_ name: String) {
+      self.isChecked = false
+      self.name = name
+    }
   }
 }
