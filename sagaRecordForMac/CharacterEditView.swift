0495ead8f7f499467329c872243f090eecc50ad0
yuta-yama-1123 <yutaforios@gmail.com>
2022-10-21 03:26:57 +0900

    10/21_キャラ編集画面追加

---
+++ sagaRecordForMac/CharacterEditView.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterEditView.swift
+//  sagaRecordForMac
+//
+//  Created by 山本祐太 on 2022/10/21.
+//
+
+import SwiftUI
+
+struct CharacterEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CharacterEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterEditView()
+    }
+}
