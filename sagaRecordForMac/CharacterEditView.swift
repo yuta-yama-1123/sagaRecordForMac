33edf3bd63b45ed2e124a9b074f9aea98d73ca50
yuta-yama-1123 <yutaforios@gmail.com>
2022-10-22 01:34:38 +0900

    10/22_履歴のためのコミット

--- sagaRecordForMac/CharacterEditView.swift
+++ sagaRecordForMac/CharacterEditView.swift
@@ -12,12 +12,31 @@ struct CharacterEditView: View {
   @Binding var editingCharacter: Bool
   
   @State var name: String = ""
+  @State var series: String = ""
+  @State var gender: String = ""
+  
   var body: some View {
     VStack {
       Text("キャラクター編集")
       HStack {
-        TextField("サインインID", text: $name)
+        TextField("キャラクター名", text: $name)
           .accessibility(identifier: "name")
+          .frame(width: 300)
+          .padding()
+          .font(
+            .system(size: 18, weight: .heavy, design: .rounded)
+          )
+        
+        TextField("シリーズ", text: $series)
+          .accessibility(identifier: "name")
+          .frame(width: 300)
+          .padding()
+          .font(
+            .system(size: 18, weight: .heavy, design: .rounded)
+          )
+        
+        TextField("性別", text: $gender)
+          .accessibility(identifier: "gender")
           .frame(width: 300)
           .padding()
           .font(
